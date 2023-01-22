581f20ba1e9a7b2756762cbf19726ab9ba199271
Omer Ali Malik <omeralimalik96@gmail.com>
2023-01-22 16:18:16 +0500

    added custom scrollbars and animated logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import bgVid from "./assets/stars.mp4"
 import TypeWriterEffect from 'react-typewriter-effect';
+import Lottie from 'react-lottie';
 
 
 
@@ -32,7 +33,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       dark : true,
+      isStopped: false, isPaused: false
     };
+    this.ai_ref = React.createRef();
   }
 
   toggleDarkMode(){
@@ -47,6 +50,15 @@ class App extends React.Component {
   }
 
   render(){
+    const defaultOptions = {
+      loop: true,
+      autoplay: true, 
+      animationData: "https://assets5.lottiefiles.com/packages/lf20_eKikFJ.json",
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
     return (
       <ThemeProvider theme={this.state.dark ? darkTheme : lightTheme}>
         <CssBaseline />
@@ -55,15 +67,17 @@ class App extends React.Component {
               <div className="overlay"></div>
               <video src={bgVid} autoPlay loop muted />
               <div className="content">
+                <iframe src="https://embed.lottiefiles.com/animation/86808" className="logo" allowTransparency="true"></iframe>
                   <h1 style={{color : "#9D00FF"}}>Scientifica</h1>
                   <div className='typewriter'>
                     <TypeWriterEffect
-                      textStyle={{ fontFamily: 'Josefin sans', color: "white" }}
-                      startDelay={100}
+                      textStyle={{ fontFamily: 'Josefin sans', color: "white" , textAlign : "center", }}
+                      startDelay={700}
                       id="subheading"
                       cursorColor="#9D00FF"
-                      text="The Best Science Magazine."
-                      typeSpeed={100}
+                      text="Science Magazines reimagined."
+                      typeSpeed={200}
+                      
                       scrollArea={document.querySelector('.content')}
                     />
                   </div>
@@ -71,11 +85,22 @@ class App extends React.Component {
                   <Button 
                     variant="contained" 
                     sx={{marginTop : 2 , fontSize : 'medium' , paddingLeft : 3 , paddingRight : 3 }}
+                    onClick={() => {
+                      this.ai_ref.current?.scrollIntoView({behavior : 'smooth'})
+                    }}
                   >Explore ↓</Button>
               </div>
           </div>
-          <div>
-            <h1>This is content</h1>
+          <div className='ai' ref={this.ai_ref}>
+            
+            {/* <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+              isStopped={this.state.isStopped}
+              isPaused={this.state.isPaused}/> */}
+              <iframe src="https://embed.lottiefiles.com/animation/9944" className='lottie'   
+              ></iframe>
+              <h1>AI</h1>
           </div>
         </main>
       </ThemeProvider> 
